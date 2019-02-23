ResurrectionRemix-Devices/device_xiaomi_kenzo

kenzo: disable sco enahnced command
 * Adjust flag after https://github.com/LineageOS/android_system_bt/commit/540b42ceaab148e32a5881b7cdf0c7872ed14b13

Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

bluetooth/bdroid_buildcfg.h
@@ -32,6 +32,6 @@
 #define BLE_PERIPHERAL_ADV_NAME  FALSE
 #define BTM_LE_SECURE_CONN  TRUE
 #define BT_CLEAN_TURN_ON_DISABLED 1
-#define BTM_SCO_ENHANCED_SYNC_DISABLED TRUE
+#define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
 
 #endif

YoDevil/android_device_xiaomi_msm8956-common

Change-Id: I8d2d47a7f39d5ec9f9a3d948c5b599ef7aae73da

kenzo/bluetooth/bdroid_buildcfg.h
@@ -34,5 +34,5 @@
 #define BT_CLEAN_TURN_ON_DISABLED 1
 #define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
 #define BTIF_HF_WBS_PREFERRED TRUE  /* Use WBS    */
-#define BTM_SCO_ENHANCED_SYNC_DISABLED TRUE
+#define BTM_SCO_ENHANCED_SYNC_DISABLED FALSE
 #endif